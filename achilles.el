7cc3bc73f9a0f0a5c9682a962c438c4159a1e015
Achilles Xu <formalin14@gmail.com>
2013-07-24 13:20:16 +0800

    add sass-mode'

--- achilles.el
+++ achilles.el
@@ -133,3 +133,6 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; for sass
+(require 'sass-mode)
